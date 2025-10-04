93ac10e18f051a8700b27c2c584bf6f0e22c42ab
kiruhadev <kirillcimbalaru@gmail.com>
2025-10-04 17:08:44 +0200

    update build

--- public/js/deposit.js
+++ public/js/deposit.js
@@ -9,7 +9,7 @@
 /* === CONFIG === */
 const MIN_DEPOSIT_TON = 0.1;                         // ← минимум на тест
 const MANIFEST_URL    = `${location.origin}/tonconnect-manifest.json?v=${Date.now()}`;
-const RECEIVER_TON    = "PASTE_YOUR_TON_ADDRESS_HERE"; // ← вставь адрес проекта (EQ...)
+const RECEIVER_TON    = "UQCtVhhBFPBvCoT8H7szNQUhEvHgbvnX50r8v6d8y5wdr19J"; // ← вставь адрес проекта (EQ...)
 
 /* === DOM === */
 const sheet       = document.getElementById("depositSheet");
